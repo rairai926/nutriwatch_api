72c0cd78e8ec96e13ca5356510c149d7d031e1cd
rairai926 <kiriyan.zero@gmail.com>
2026-02-18 13:52:43 +0800

    delete

--- admin/some_admin_api.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../auth/require_role.php';
-
-$user = requireRole(['admin']); // ✅ only CSWD staff
-
-echo json_encode(["message" => "Admin OK", "user" => $user]);
--- users/profie.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../auth/auth.php';
-require '../config/db.php';
-
-echo json_encode([
-    "message" => "You are authenticated",
-    "user" => $authUser
-]);
