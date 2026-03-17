586f30f931d67866244c5e71a9e300657e469699
rairai926 <kiriyan.zero@gmail.com>
2026-03-17 23:30:16 +0800

    preview_measurement

--- child_mobile/preview_measurement.php
+++ child_mobile/preview_measurement.php
@@ -2,9 +2,6 @@
 ob_start();
 session_start();
 
-header("Content-Type: application/json; charset=utf-8");
-ini_set('display_errors', 0);
-error_reporting(E_ALL);
 
 require_once __DIR__ . "/../config/db.php";
 require_once __DIR__ . "/../middleware/auth.php";
